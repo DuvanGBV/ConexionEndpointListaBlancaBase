6eca804643c429cf788167cc69af2a2954563cbb
DuvanGBV <152649907+DuvanGBV@users.noreply.github.com>
2024-08-02 17:25:44 -0500

    proyecto

---
+++ app/src/main/java/com/example/listablancabase/MainActivity.kt
@@ -0,0 +1,89 @@
+package com.example.listablancabase
+
+import android.content.Context
+import android.content.SharedPreferences
+import android.os.Bundle
+import android.util.Log
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.listablancabase.ui.theme.ListaBlancaBaseTheme
+import java.io.File
+import java.io.FileOutputStream
+import java.io.IOException
+import java.io.OutputStream
+import java.net.HttpURLConnection
+import java.net.URL
+import java.util.zip.GZIPInputStream
+
+class MainActivity : ComponentActivity() {
+    private val TAG = "MainActivity"
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+
+        downloadWhitelistBase(this)
+        setContent {
+            ListaBlancaBaseTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+
+    private fun downloadWhitelistBase(context: Context) {
+        val url = URL("https://5khbta9jbk.execute-api.us-west-2.amazonaws.com/base_wl")
+        val outputFile = File(context.filesDir, "whitelist_temp.txt") // Usar context.filesDir
+
+        try {
+            val connection = url.openConnection() as HttpURLConnection
+            connection.connect()
+
+            if (connection.responseCode == HttpURLConnection.HTTP_OK) {
+                val inputStream = connection.inputStream
+                val gzInputStream = GZIPInputStream(inputStream)
+                val outputStream: OutputStream = FileOutputStream(outputFile)
+
+                val buffer = ByteArray(1024)
+                var length: Int
+                while (gzInputStream.read(buffer).also { length = it } > 0) {
+                    outputStream.write(buffer, 0, length)
+                }
+
+                gzInputStream.close()
+                outputStream.close()
+                inputStream.close()
+            }
+        } catch (e: IOException) {
+            Log.e(TAG, "Error downloading and updating whitelist", e)
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    ListaBlancaBaseTheme {
+        Greeting("Android")
+    }
+}
